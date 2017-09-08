eac45f5d5f3a3fba704e4fe572bc9655e69198a0
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2017-09-08 14:39:50 -0300

    se agrega ordenamiento numerico por version soportando versiones incompletas

--- server/table-modules.js
+++ server/table-modules.js
@@ -13,10 +13,10 @@ module.exports = function (context) {
             { name: 'codenautas_owned', typeName: 'boolean', title: 'es de codenautas?' },
             { name: 'group', typeName: 'text', title: 'grupo' },
             { name: 'class', typeName: 'text', title: 'clase' },
-            { name: 'version', typeName: 'text', title: 'última versión', inTable: false , editable:false},
-            { name: 'cobertura', typeName: 'text', title: 'cobertura últ. versión', inTable: false , editable:false},
-            { name: 'test_pass', typeName: 'boolean', title: 'pasan test en últ. versión?', inTable: false , editable:false},
-            { name: 'insecurity_level', typeName: 'text', title: 'nivel de inseg. de últ. versión', inTable: false , editable:false},
+            { name: 'version', typeName: 'text', title: 'última versión', inTable: false, editable: false },
+            { name: 'cobertura', typeName: 'text', title: 'cobertura últ. versión', inTable: false, editable: false },
+            { name: 'test_pass', typeName: 'boolean', title: 'pasan test en últ. versión?', inTable: false, editable: false },
+            { name: 'insecurity_level', typeName: 'text', title: 'nivel de inseg. de últ. versión', inTable: false, editable: false },
         ],
         primaryKey: ['module'],
         detailTables: [
@@ -26,7 +26,7 @@ module.exports = function (context) {
             isTable: true,
             // from: `(select m.*, vv.version, vv.cobertura, vv.test_pass, vv.insecurity_level from modules m, lateral (select v.* from versions v where m.module = v.module order by v.version desc limit 1) vv)`
             // from: `(select m.* from modules m, lateral (select v.* from versions v where m.module = v.module order by v.version desc limit 1) vv)`
-            from: `(select * from modules m left join (select distinct on (v.module) v.version, v.module version_module, v.cobertura, v.test_pass, v.insecurity_level from versions v order by v.module, v.version desc) v on (m.module = v.version_module) )`
+            from: `(select * from modules m left join (select distinct on (v.module) v.version, v.module version_module, v.cobertura, v.test_pass, v.insecurity_level from versions v order by v.module, string_to_array(v.version,'.')::int[] desc) v on (m.module = v.version_module))`
         }
     }, context);
 }
