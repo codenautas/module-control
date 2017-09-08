6f62a74c4c07a5bf6ad9832f653733f6d922fbf9
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2017-09-08 13:41:13 -0300

    agrego a la vista de módulos la info de la última versión de cada módulo

--- server/table-modules.js
+++ server/table-modules.js
@@ -12,11 +12,21 @@ module.exports = function (context) {
             { name: 'importance', typeName: 'integer', title: 'importancia' },
             { name: 'codenautas_owned', typeName: 'boolean', title: 'es de codenautas?' },
             { name: 'group', typeName: 'text', title: 'grupo' },
-            { name: 'class', typeName: 'text', title: 'clase' }
+            { name: 'class', typeName: 'text', title: 'clase' },
+            { name: 'version', typeName: 'text', title: 'última versión', inTable: false , editable:false},
+            { name: 'cobertura', typeName: 'text', title: 'cobertura últ. versión', inTable: false , editable:false},
+            { name: 'test_pass', typeName: 'boolean', title: 'pasan test en últ. versión?', inTable: false , editable:false},
+            { name: 'insecurity_level', typeName: 'text', title: 'nivel de inseg. de últ. versión', inTable: false , editable:false},
         ],
         primaryKey: ['module'],
         detailTables: [
-            {table: 'versions'      , fields:['module'], abr:'V', label:'versiones'},
-        ]
+            { table: 'versions', fields: ['module'], abr: 'V', label: 'versiones' },
+        ],
+        sql: {
+            isTable: true,
+            // from: `(select m.*, vv.version, vv.cobertura, vv.test_pass, vv.insecurity_level from modules m, lateral (select v.* from versions v where m.module = v.module order by v.version desc limit 1) vv)`
+            // from: `(select m.* from modules m, lateral (select v.* from versions v where m.module = v.module order by v.version desc limit 1) vv)`
+            from: `(select * from modules m left join (select distinct on (v.module) v.version, v.module version_module, v.cobertura, v.test_pass, v.insecurity_level from versions v order by v.module, v.version desc) v on (m.module = v.version_module) )`
+        }
     }, context);
 }
